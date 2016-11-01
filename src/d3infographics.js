1f438809c34f4db48f6a0702204cd7dda4a84bd3
pratyush <pratyushtamhankar@gmail.com>
2016-11-01 12:00:38 +0530

    Bug fix while converting hex colors to rgba.

--- src/d3infographics.js
+++ src/d3infographics.js
@@ -410,11 +410,11 @@
         else if (color.indexOf('#') === 0) {
             // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
             var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-            hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+            color = color.replace(shorthandRegex, function (m, r, g, b) {
                 return r + r + g + g + b + b;
             });
 
-            var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+            var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(color);
             return result
                 ? 'rgba(' + parseInt(result[1], 16).toString() + ',' + parseInt(result[2], 16).toString() + ',' + parseInt(result[3], 16).toString() + ',' + opacity + ')'
                 : 'rgba(255, 255, 255, ' + opacity + ')';
